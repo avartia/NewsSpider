84fcaf293c7999212cfe9dd8e0895f9d86746a54
avartia <903703465@qq.com>
2017-06-30 14:06:22 +0800

    定时调用

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 # created by avartialu@gmail.com on 2017/6/20
 
 from scrapy import cmdline
+from threading import Timer
 
-if __name__ == "__main__":
+
+def excecute_spider(inc):
     cmdline.execute("scrapy crawl sina-news".split())
     cmdline.execute("scrapy crawl education".split())
     cmdline.execute("scrapy crawl entertainment".split())
     cmdline.execute("scrapy crawl sports".split())
     cmdline.execute("scrapy crawl finance".split())
     cmdline.execute("scrapy crawl science".split())
+    t = Timer(inc, excecute_spider, (inc, ))
+    t.start()
+
+if __name__ == "__main__":
+    # 每隔一个小时调用一次
+    excecute_spider(3600)
